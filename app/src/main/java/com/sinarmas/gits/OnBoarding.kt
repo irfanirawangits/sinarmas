59198c29c9f944091032da5218a218d99a42ee1f
Ryvaldie <ryvaldiehamdallah@gmail.com>
2018-07-02 14:45:11 +0700

    Initial commit

---
+++ app/src/main/java/com/sinarmas/gits/OnBoarding.kt
@@ -0,0 +1,12 @@
+package com.sinarmas.gits
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class OnBoarding : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_on_boarding)
+    }
+}
